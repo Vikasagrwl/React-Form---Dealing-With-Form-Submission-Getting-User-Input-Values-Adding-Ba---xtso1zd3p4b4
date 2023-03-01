f584db464abbf2a15a7579ec64a4983fbed2b8b0
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-01 10:28:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,12 +18,11 @@ const App = () => {
     contactNo: "",
   });
 
-  const[flag,setFlag] = useState(false);
+  const [flag, setFlag] = useState(false);
+  
   const formSubmit = (e) => {
     e.preventDefault();
     const validationResult = Validation(details);
-
-    console.log(validationResult);
     if (Object.keys(validationResult).length > 0) {
       setError(validationResult);
       setDetails({
@@ -33,8 +32,7 @@ const App = () => {
         contactNo: "",
       });
       return;
-    } 
-    else {
+    } else {
       setFlag(true);
       setDetails({
         username: "",
@@ -47,39 +45,52 @@ const App = () => {
         email: "",
         password: "",
         contactNo: "",
-      })
+      });
     }
   };
-
-  
 
   const handleChange = (e) => {
     setDetails({ ...details, [e.target.name]: e.target.value });
   };
 
-
   return (
     <div id="main">
-      {flag ? (
-        <h3 className="success-alert">Registered Successfullly</h3>
-      ) : (
-        ""
-      )}
+      {flag ? <h3 className="success-alert">Registered Successfullly</h3> : ""}
       <form onSubmit={formSubmit}>
         <h1>Registeration Form</h1>
         <section>
           <label>Username</label>
-          <input type="text" name="username" value={details.username} onChange={handleChange} />
-          {!flag? <p className="username-error">{error.username}</p> :""}
+          <input
+            type="text"
+            name="username"
+            value={details.username}
+            onChange={handleChange}
+          />
+          {error.username.length>0 ? (<p className="username-error">{error.username}</p> ): ("")}
           <label>Email</label>
-          <input type="email" name="email" value={details.email} onChange={handleChange} />
-          {!flag? <p className="email-error">{error.email}</p> : ""}
+          <input
+            type="email"
+            name="email"
+            value={details.email}
+            onChange={handleChange}
+          />
+          {error.email.length>0 ? <p className="email-error">{error.email}</p> : ""}
           <label>Password</label>
-          <input type="password" name="password" value={details.password} onChange={handleChange} />
-          {!flag? <p className="password-error">{error.password}</p> :""}
+          <input
+            type="password"
+            name="password"
+            value={details.password}
+            onChange={handleChange}
+          />
+          {error.password.length>0 ? <p className="password-error">{error.password}</p> : ""}
           <label>Contact Number</label>
-          <input type="number" name="contactNo" value={details.contactNo} onChange={handleChange} />
-          {!flag? <p className="contactNo-error">{error.contactNo}</p> :""}
+          <input
+            type="number"
+            name="contactNo"
+            value={details.contactNo}
+            onChange={handleChange}
+          />
+          {error.contactNo.length>0 ? <p className="contactNo-error">{error.contactNo}</p> : ""}
           <button type="submit">Submit</button>
         </section>
       </form>
